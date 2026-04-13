ee2c0861a71af8d3d670a3f6a3031078290ebff5
PrimeBuild-pc <primebuild.official@gmail.com>
2026-04-13 12:15:46 +0200

    update workflows

--- scripts/build-release.ps1
+++ scripts/build-release.ps1
@@ -2,7 +2,7 @@
 # This script builds optimized portable releases for x64 and ARM64 architectures
 
 param(
-    [string]$Version = "1.2.0",
+    [string]$Version = "1.3.0",
     [switch]$SkipClean = $false,
     [switch]$SelfContained,
     [switch]$Trim
